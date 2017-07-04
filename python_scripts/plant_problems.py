d42e55c691485d32ca52564334428e784c9ab41f
Brad Wenner <brad@bradwenner.com>
2017-07-04 11:49:53 -0700

    Dynamic icon and more attributes

--- python_scripts/plant_problems.py
+++ python_scripts/plant_problems.py
@@ -1,13 +1,16 @@
 problemPlants = 0
+allproblemPlants = []
 waterPlants = []
 numberWater = 0
 fertilizePlants = []
 numberFertilize = 0
+whichIcon = "mdi:help-circle-outline"
 
 for entity_id in hass.states.entity_ids('plant'):
     state = hass.states.get(entity_id)
     if state.state == 'problem':
         problemPlants = problemPlants + 1
+        allproblemPlants.append(state.name)
         problem = state.attributes.get('problem') or 'none'
         if "conductivity low" in problem:
           fertilizePlants.append(state.name)
@@ -15,10 +18,19 @@ for entity_id in hass.states.entity_ids('plant'):
         if "moisture low" in problem:
           waterPlants.append(state.name)
           numberWater = numberWater + 1
+
+# Set icon
+if problemPlants > 0:
+  whichIcon = "mdi:alert-circle-outline"
+else:
+  whichIcon = "mdi:check-circle-outline"
+
 # Set states
 hass.states.set('sensor.water_plants', problemPlants, {
     'unit_of_measurement': 'plants',
     'friendly_name': 'Problem Plants',
+    'icon': whichIcon,
+    'problem_plants': allproblemPlants,
     'water': waterPlants,
     'water_number': numberWater,
     'fertilize': fertilizePlants,
