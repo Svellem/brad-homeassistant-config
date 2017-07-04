e4cec1529651637026770fe03561ad49d04c866e
Brad Wenner <brad@bradwenner.com>
2017-07-04 11:35:26 -0700

    Plant Problem script done

--- python_scripts/plant_problems.py
+++ python_scripts/plant_problems.py
@@ -1,12 +1,26 @@
 problemPlants = 0
+waterPlants = []
+numberWater = 0
+fertilizePlants = []
+numberFertilize = 0
 
-# For every plant
 for entity_id in hass.states.entity_ids('plant'):
     state = hass.states.get(entity_id)
     if state.state == 'problem':
         problemPlants = problemPlants + 1
+        problem = state.attributes.get('problem') or 'none'
+        if "conductivity low" in problem:
+          fertilizePlants.append(state.name)
+          numberFertilize = numberFertilize + 1
+        if "moisture low" in problem:
+          waterPlants.append(state.name)
+          numberWater = numberWater + 1
 # Set states
 hass.states.set('sensor.water_plants', problemPlants, {
     'unit_of_measurement': 'plants',
-    'friendly_name': 'Plants with Problems',
+    'friendly_name': 'Problem Plants',
+    'water': waterPlants,
+    'water_number': numberWater,
+    'fertilize': fertilizePlants,
+    'fertilize_number': numberFertilize
 })
